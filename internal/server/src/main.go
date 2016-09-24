7880a14e2ae2d6f85b73eb6b11796b64f65d3022
corvuscrypto <cmrallen@gmail.com>
2016-09-23 21:26:29 -0700

    finished the basic card style

--- internal/server/src/main.go
+++ internal/server/src/main.go
@@ -31,6 +31,15 @@ func getRouter() *httprouter.Router {
 	router.ServeFiles("/static/*filepath", http.Dir(Config.StaticPath))
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		data := BaseData(r, "CorvusCrypto.com - just another coder blog")
+
+		data["LatestPost"] = struct {
+			Title,
+			Summary string
+		}{
+			"Test title for great justice!",
+			"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		}
+
 		err := globalTemplate.ExecuteTemplate(w, "index", data)
 		if err != nil {
 			fmt.Println(err)

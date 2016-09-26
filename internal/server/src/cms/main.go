1f0b331ef5f689d33b6f2619839ad2ed828ca52c
corvuscrypto <cmrallen@gmail.com>
2016-09-26 07:04:04 -0700

    added route for base draft list

--- internal/server/src/cms/main.go
+++ internal/server/src/cms/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func getRouter() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("", checkAuth(func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		err := globalTemplate.ExecuteTemplate(w, "drafts", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}))
+	return router
+}
 
 func main() {
+
 	server := new(http.Server)
 	server.Addr = ":8081"
 	server.ListenAndServeTLS("", "")
